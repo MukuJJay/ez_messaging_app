e3b2ad339dcc22000dd251b9594276bc7faa09e6
Mukul <mukujjee@gmail.com>
2023-11-28 00:55:10 +0530

    receive message needs fixing

--- backend/controllers/send-receiveMessage.js
+++ backend/controllers/send-receiveMessage.js
@@ -21,26 +21,96 @@ export const sendMessage = async (
     messageId: message._id,
   });
 
-  const allConvo = await Conversation.find({
-    $or: [
-      { senderId: senderIdObj, receiverId: receiverIdObj },
-      { senderId: receiverIdObj, receiverId: senderIdObj },
-    ],
-  }).populate("messageId");
+  // const allConvo = await Conversation.find({
+  //   $or: [
+  //     { senderId: senderIdObj, receiverId: receiverIdObj },
+  //     { senderId: receiverIdObj, receiverId: senderIdObj },
+  //   ],
+  // }).populate("messageId");
 
-  io.emit("allConvo", allConvo);
+  // const allConvo = await Conversation.aggregate([
+  //   {
+  //     $match: {
+  //       $expr: {
+  //         $or: [
+  //           { senderId: senderIdObj, receiverId: receiverIdObj },
+  //           { senderId: receiverIdObj, receiverId: senderIdObj },
+  //         ],
+  //       },
+  //     },
+  //   },
+  //   {
+  //     $lookup: {
+  //       from: "messages",
+  //       let: { messageId: "$messageId" },
+  //       pipeline: [
+  //         {
+  //           $match: {
+  //             $expr: {
+  //               $and: [{ $eq: ["$_id", "$$messageId"] }],
+  //             },
+  //           },
+  //         },
+  //       ],
+  //       as: "message_details",
+  //     },
+  //   },
+  //   {
+  //     $unwind: {
+  //       path: "$message_details",
+  //       preserveNullAndEmptyArrays: true,
+  //     },
+  //   },
+  // ]);
+
+  // io.emit("allConvo", allConvo);
 };
 
 export const receiveMessage = async (req, res) => {
   const token = req.headers.authorization.slice(7).trim();
   const userId = jwt.verify(token, process.env.JWT_SECRET).id;
   const receiverId = req.body.receiverId;
-  const allConvo = await Conversation.find({
-    $or: [
-      { senderId: userId, receiverId: receiverId },
-      { senderId: receiverId, receiverId: userId },
-    ],
-  }).populate("messageId");
+  // const allConvo = await Conversation.find({
+  //   $or: [
+  //     { senderId: userId, receiverId: receiverId },
+  //     { senderId: receiverId, receiverId: userId },
+  //   ],
+  // }).populate("messageId");
+
+  const allConvo = await Conversation.aggregate([
+    {
+      $match: {
+        $expr: {
+          $or: [
+            { senderId: userId, receiverId: receiverId },
+            { senderId: receiverId, receiverId: userId },
+          ],
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "messages",
+        let: { messageId: "$messageId" },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $and: [{ $eq: ["$_id", "$$messageId"] }],
+              },
+            },
+          },
+        ],
+        as: "message_details",
+      },
+    },
+    {
+      $unwind: {
+        path: "$message_details",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+  ]);
 
   res.status(200).json({
     status: 200,
